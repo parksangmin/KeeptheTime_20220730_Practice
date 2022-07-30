44305b69279bf72c7dee0952251edf8e413b3adf
박 상 민 <dhwkffks7@naver.com>
2022-07-30 19:21:12 +0900

    Initial commit

---
+++ app/src/main/java/com/sangmin/keepthetime_20220730_practice/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.sangmin.keepthetime_20220730_practice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
