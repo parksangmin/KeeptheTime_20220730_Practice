8290bb899683c25afc567ec5da877c6e12ab433c
박 상 민 <dhwkffks7@naver.com>
2022-07-30 19:26:42 +0900

    BaseActivity.kt 생성 및 로그인 액티비티 kt파일 기초 작업

--- app/src/main/java/com/sangmin/keepthetime_20220730_practice/LoginActivity.kt
+++ app/src/main/java/com/sangmin/keepthetime_20220730_practice/LoginActivity.kt
@@ -2,10 +2,25 @@ package com.sangmin.keepthetime_20220730_practice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.sangmin.keepthetime_20220730_practice.databinding.ActivityLoginBinding
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : BaseActivity() {
+
+    lateinit var mBinding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
     }
 }
