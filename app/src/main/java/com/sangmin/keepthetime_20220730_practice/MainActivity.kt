e413504aa859dcfebf10c7a1f7de2d9512073c3f
박 상 민 <dhwkffks7@naver.com>
2022-08-02 15:36:18 +0900

    뷰페이저 / bottomNav 연동 event 처리

--- app/src/main/java/com/sangmin/keepthetime_20220730_practice/MainActivity.kt
+++ app/src/main/java/com/sangmin/keepthetime_20220730_practice/MainActivity.kt
@@ -3,8 +3,10 @@ package com.sangmin.keepthetime_20220730_practice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.viewpager2.widget.ViewPager2
 import com.sangmin.keepthetime_20220730_practice.adapters.MainViewPagerAdapter
 import com.sangmin.keepthetime_20220730_practice.databinding.ActivityMainBinding
+import retrofit2.Callback
 
 class MainActivity : BaseActivity() {
 
@@ -27,6 +29,28 @@ class MainActivity : BaseActivity() {
     override fun setValues() {
         mPagerAdapter = MainViewPagerAdapter(this)
         mBinding.mainViewPager.adapter = mPagerAdapter
+
+
+//         뷰페이저 연동 event
+        mBinding.mainViewPager.registerOnPageChangeCallback(
+            object : ViewPager2.OnPageChangeCallback(){
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    mBinding.bottomNav.menu.getItem(position).isChecked = true
+                }
+            }
+        )
+
+//        bottomNav 연동 event
+        mBinding.bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.calendar -> mBinding.mainViewPager.currentItem = 0
+                R.id.invite -> mBinding.mainViewPager.currentItem = 1
+                R.id.setting -> mBinding.mainViewPager.currentItem = 2
+            }
+            return@setOnItemSelectedListener true
+
+        }
 
     }
 }
